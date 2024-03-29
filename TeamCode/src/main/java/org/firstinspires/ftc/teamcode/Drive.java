b507e3ee859ef5c53aa823b988ac7ad3652057b4
wilcoxrobotics <teamrobotics8872@gmail.com>
2024-03-29 16:57:59 -0700

    Drive,Slides,intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Drive extends LinearOpMode {
@@ -11,13 +13,28 @@ public class Drive extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        DcMotor liftRight = hardwareMap.dcMotor.get("liftRight");
+        DcMotor liftLeft = hardwareMap.dcMotor.get("liftLeft");
+        DcMotor intakeMotor = hardwareMap.get(DcMotor.class,"intakeMotor");
+        Servo arm = hardwareMap.get(Servo.class,"arm");
+        Servo claw = hardwareMap.get(Servo.class,"claw");
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad previousGamepad1 = new Gamepad();
 
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+//        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+//        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
@@ -27,18 +44,49 @@ public class Drive extends LinearOpMode {
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
+            double liftPower = 0.5;
+            double intakeMotorPower = 0.5;
+            previousGamepad1.copy(currentGamepad1);
+            currentGamepad1.copy(gamepad1);
 
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
+            double frontLeftPower = (y + x - rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            double backRightPower = (y + x + rx) / denominator;
 
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
+            if (gamepad1.a) {
+                liftRight.setPower(liftPower);
+                liftLeft.setPower(-liftPower);
+            } else {
+                liftRight.setPower(0);
+                liftLeft.setPower(0);
+            }
+            if (gamepad1.b) {
+                liftRight.setPower(-liftPower);
+                liftLeft.setPower(liftPower);
+            } else {
+                liftRight.setPower(0);
+                liftLeft.setPower(0);
+            }
+            if (gamepad1.left_bumper) {
+                intakeMotor.setPower(-intakeMotorPower);
+            } else if (gamepad1.right_bumper){
+                intakeMotor.setPower(intakeMotorPower);
+            } else {
+                intakeMotor.setPower(0);
+            }
+
+            if (gamepad1.x) {
+                arm.setPosition(arm.getPosition() + 0.1);
+            }
+
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
         }
     }
 }
